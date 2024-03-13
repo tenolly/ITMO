c64359509b674fc5978631225991970784ca8660
tenolly <np50022005@yandex.ru>
2024-03-13 14:19:37 +0300

    update folders

--- semester 2/programming/lab5/README.md
+++
@@ -1,7 +0,0 @@
-<img src="media/task1.jpg">
-<img src="media/task2.jpg">
-
-## Run
-```bash
-./gradlew run --console=plain
-```
--- semester 2/programming/lab5/app/src/main/java/lab5/commands/core/TerminateException.java
+++
@@ -1,8 +0,0 @@
-package lab5.commands.core;
-
-import com.google.common.util.concurrent.UncheckedExecutionException;
-
-/**
-* Exception using when user force to terminate command
-*/
-public class TerminateException extends UncheckedExecutionException {}
