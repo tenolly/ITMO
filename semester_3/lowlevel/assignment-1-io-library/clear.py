7de55b62c2ebe4a291e5d054eee0c1837eb41b98
Nikita Pyshkin <68381689+tenolly@users.noreply.github.com>
2024-11-14 18:59:41 +0300

    Delete semester_3/lowlevel directory

--- semester_3/lowlevel/assignment-1-io-library/clear.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-for filename in os.listdir():
-    if (filename.startswith("parse_") or filename.startswith("string_") or 
-        filename.startswith("read_") or filename.startswith("print_") or
-        filename == "report.xml"):
-        os.remove(filename)
